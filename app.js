1eb7fd57e28d3d1622d152622e51517bbc70146d
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-11-06 17:24:00 -0300

    :goal_net: Fazendo validações para não chegarem informações vazias na API.

--- app.js
+++ app.js
@@ -7,12 +7,39 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+const User = require('./models/User')
+
 app.get('/', (req, res) => {
     res.status(200).json({ msg: "Api-Test" });
 });
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
+
+// Parte de Registro de usuário
+app.post('/auth/register', async(req, res) => {
+    const { name, email, password, confirmpassword } = req.body;
+
+
+    //Validações
+    if(!name) {
+        return res.status(422).json({msg: "o nome é obrigatório!"})
+    }
+
+    if(!email) {
+        return res.status(422).json({msg: "o email é obrigatório!"})
+    }
+
+    if(!password) {
+        return res.status(422).json({msg: "a senha é obrigatória!"})
+    }
+
+    if(password !== confirmpassword) {
+        return res.status(422).json({msg: "As senhas não são iguais!"})
+    }
+
+})
+
 
 mongoose
     .connect(`mongodb+srv://${dbUser}:${dbPass}@jwtappjson.wrtez.mongodb.net/?retryWrites=true&w=majority&appName=JWTAPPJson`)
