d699326545c1fd3a3a96a78f753d8324f40e12cc
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-11-07 14:15:03 -0300

    :sparkles: feat: Registro de usuarios e senha em hash.

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
-const User = require('./models/User')
+// Middleware para interpretar JSON
+app.use(express.json());
+
+const User = require('./models/User');
 
 app.get('/', (req, res) => {
     res.status(200).json({ msg: "Api-Test" });
@@ -16,30 +19,51 @@ app.get('/', (req, res) => {
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
-// Parte de Registro de usuário
-app.post('/auth/register', async(req, res) => {
+app.post('/auth/register', async (req, res) => {
     const { name, email, password, confirmpassword } = req.body;
 
-
-    //Validações
-    if(!name) {
-        return res.status(422).json({msg: "o nome é obrigatório!"})
+    // Validações
+    if (!name) {
+        return res.status(422).json({ msg: "o nome é obrigatório!" });
     }
 
-    if(!email) {
-        return res.status(422).json({msg: "o email é obrigatório!"})
+    if (!email) {
+        return res.status(422).json({ msg: "o email é obrigatório!" });
     }
 
-    if(!password) {
-        return res.status(422).json({msg: "a senha é obrigatória!"})
+    if (!password) {
+        return res.status(422).json({ msg: "a senha é obrigatória!" });
     }
 
-    if(password !== confirmpassword) {
-        return res.status(422).json({msg: "As senhas não são iguais!"})
+    if (password !== confirmpassword) {
+        return res.status(422).json({ msg: "As senhas não são iguais!" });
     }
 
-})
+    // Verifica se já existe um usuário com esse email
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+        return res.status(422).json({ msg: "Já existe um usuário com esse email!" });
+    }
 
+    // Criando a senha / Criptografia da Senha
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Criando User
+    const user = new User({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    try {
+        await user.save();
+        res.status(201).json({ msg: "Usuário registrado com sucesso!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Houve um erro ao registrar o usuário!" });
+    }
+});
 
 mongoose
     .connect(`mongodb+srv://${dbUser}:${dbPass}@jwtappjson.wrtez.mongodb.net/?retryWrites=true&w=majority&appName=JWTAPPJson`)
